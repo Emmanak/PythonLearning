baad9967a92c89d25d5e5c7df2f4993e6c7584ae
Emmanak <emmanuelaugustine@hotmail.com>
2018-07-11 08:12:19 +0000

    Tutorial: Lists

---
+++ hello.py
@@ -0,0 +1,19 @@
+import random
+import sys
+import os
+
+print("Hello Mannie")
+
+name = "Emmanuel"
+
+print"my name is" ,name
+
+grocery_list = ['peanutbutter', 'bananas','food', 'drink']
+
+print grocery_list
+
+print 'I like to eat' ,grocery_list[1] ,'with' ,grocery_list[0]
+
+print( str(grocery_list[1:2]))
+
+print(len(grocery_list))
