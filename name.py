547f75b2bd4e06acb4684f0bce83b2232088eca2
Emmanak <emmanuelaugustine@hotmail.com>
2018-08-08 00:09:04 +0000

    name and hello with objects

---
+++ name.py
@@ -0,0 +1,12 @@
+import random
+import sys
+import os
+
+
+age=input('What is your age?\n') 
+print "you age is", age
+if age > 13: print("You must be Jerin")
+else: print("you must be Jefin")
+
+type(age)
+
